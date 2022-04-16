8ba3e58fdfd5b4b72bba93f23505a1507a6d764e
rahulpdiggi <78218847+rahulpdiggi@users.noreply.github.com>
2022-04-16 14:34:37 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "Demo" {
+  ami           = "ami-0d2986f2e8c0f7d01"
+  instance_type = "t2.micro"
+  key_name = "mytableaupoc"
+  tags = {
+    Name = "freetier_using_terraform"
+  }
+}
